3aa1e71e40551cc7c62260332caa2dbe50a24d82
arroway <stephanie@minet.net>
2012-03-14 22:45:46 +0100

    inital commit

---
+++ lib/Net/OSLC/CM.pm
@@ -0,0 +1,85 @@
+package Net::OSLC::CM;
+use Any::Moose;
+
+use Net::OSLC::CM::Connection;
+
+our $VERSION = '0.01';
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Net::OSLC ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+
+
+# Preloaded methods go here.
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Net::OSLC::CM - Interact with an OSLC Service Provider Catalog, respecting specifications of OSLC Change Management v.2
+
+=head1 SYNOPSIS
+
+  use Net::OSLC::CM;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Net::OSLC, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Stephanie Ouillon, E<lt>stephanie.ouillon@telecom-sudparis.eu<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Stephanie Ouillon
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.12.3 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ lib/Net/OSLC/CM/Connection.pm
@@ -0,0 +1,23 @@
+package Net::OSLC::CM::Connection;
+use Any::Moose;
+
+use URI;
+use LWP::Agent;
+
+has uri => (
+  isa => 'Str',
+  is  => 'ro'
+);
+
+has connection => (
+  isa => 'LWP::UserAgent', 
+  is => 'rw',
+  lazy =>1,
+  default => sub {
+    my $self = shift;
+    my $connection = LWP::UserAgent->new();
+    return $connection;
+  }
+);
+
+1;
